d18d4e55cf94138fe5b6947933e3ef7473d09e90
PoonamTrijala <punamshrestha074@gmail.com>
2022-04-11 20:20:34 +0545

    Online-Doctor-Appointment

--- static/js/main.js
+++ static/js/main.js
@@ -11,11 +11,14 @@ function validateEmail() {
         return true;
     }
 }
+
 function validateForm() {
     var inputText = document.getElementById('email').value;
     var error = document.getElementById('emailError');
     var contact= document.getElementById('contact').value;
     var contactError= document.getElementById('contactError');
+    var password= document.getElementById('passw').value;
+    var passwordError= document.getElementById('passwordError');
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!inputText.match(mailformat)) {
         error.innerHTML = "You have entered an invalid email address!";
@@ -27,12 +30,26 @@ function validateForm() {
         return false;
 
     }
-    else {
-        return true;
+    var re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    var x = document.forms["form1"]["passw"].value;
+    if (x == "") {
+        passwordError.innerHTML=("Password cannot be blank.");
+    return false;
+     }
+       
+     else if (!password.match(re)) {
+        passwordError.innerHTML="Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)";
+        return re.test(password);
+    } else {
+        document.write("valid")
+    
     }
 
 }
-let contact = document.getElementById('contact');
+
+
+
+/*let contact = document.getElementById('contact');
 let defaultValue = contact.value;
 
 contact.addEventListener('contact', checkInput);
@@ -46,4 +63,4 @@ function checkInput(e) {
     // if it is then reset the input value to the default one
     this.value = defaultValue;
   }
-}
+}*/
--- stockapp/models.py
+++ stockapp/models.py
@@ -1,8 +1,46 @@
+from aifc import _aifc_params
+from imaplib import _Authenticator
+from django import forms
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import CharField
 from django.db import models 
 import datetime
+from django.contrib.auth.models import User
+'''class AuthenticationForm(forms.Form):
+   
+    username = forms.CharField(label=("Username"), max_length=30)
+    password = forms.CharField(label=("Password"), widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user_cache = None
+        super(AuthenticationForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = _Authenticator(username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(_Authenticator("Please enter a correct username and password. Note that both fields are case-sensitive."))
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(_Authenticator("This account is inactive."))
+        if self.request:
+            if not self.request.session.test_cookie_worked():
+                raise forms.ValidationError(_Authenticator("Your Web browser doesn't appear to have cookies enabled. Cookies are required for logging in."))
+
+        return self.cleaned_data
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
+        '''
 
 class Contact(models.Model):
     name=models.CharField(max_length=250)
